97348039ed57d86c736f41f8e02bd735aad73a51
Dimitrios Chatzis <dimitrios.chatzis@unify.com>
2015-04-06 15:02:53 -0400

    Initial UI in place

---
+++ app/src/main/java/com/drones/dimis/dronehud/HUDActivity.java
@@ -0,0 +1,132 @@
+package com.drones.dimis.dronehud;
+
+
+/*
+import com.MAVLink.Messages.MAVLinkMessage;
+import com.MAVLink.Messages.ardupilotmega.msg_attitude;
+import com.MAVLink.Messages.ardupilotmega.msg_heartbeat;
+import com.MAVLink.Messages.ardupilotmega.msg_mission_current;
+import com.MAVLink.Messages.ardupilotmega.msg_nav_controller_output;
+import com.MAVLink.Messages.ardupilotmega.msg_request_data_stream;
+import com.MAVLink.Messages.ardupilotmega.msg_vfr_hud;
+import com.diydrones.droidplanner.helpers.HUDwidget;
+import com.diydrones.droidplanner.service.MAVLinkClient;
+import com.o3dr.hellodrone.widget;
+
+public class HUDActivity extends Activity {
+
+    HUDwidget hudWidget;
+    public boolean running;
+    MenuItem connectButton;
+
+    @Override
+    int getNavigationItem() {
+        return 1;
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.hud_fragment);
+
+        hudWidget = (HUDwidget) findViewById(R.id.hudWidget);
+
+        MAVClient.init();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        MAVClient.onDestroy();
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_flightdata, menu);
+        connectButton = menu.findItem(R.id.menu_connect);
+        return true;
+    }
+
+    @Override
+    public boolean onMenuItemSelected(int featureId, MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+                startActivity(new Intent(this, SettingsActivity.class));
+                return true;
+            case R.id.menu_connect:
+                MAVClient.sendConnectMessage();
+                return true;
+            default:
+                return super.onMenuItemSelected(featureId, item);
+        }
+    }
+
+    public MAVLinkClient MAVClient = new MAVLinkClient(this) {
+        @Override
+        public void notifyReceivedData(MAVLinkMessage msg) {
+            switch (msg.msgid) {
+                case msg_attitude.MAVLINK_MSG_ID_ATTITUDE:
+                    msg_attitude m = (msg_attitude) msg;
+                    hudWidget.newFlightData(m.roll, m.pitch, m.yaw);
+                    break;
+                case msg_vfr_hud.MAVLINK_MSG_ID_VFR_HUD:
+                    hudWidget.setAltitude(((msg_vfr_hud) msg).alt);
+                    hudWidget.setGroundSpeed(((msg_vfr_hud) msg).groundspeed);
+                    hudWidget.setAirSpeed(((msg_vfr_hud) msg).airspeed);
+                    break;
+                case msg_mission_current.MAVLINK_MSG_ID_MISSION_CURRENT:
+                    hudWidget.setWaypointNumber(((msg_mission_current) msg).seq);
+                    break;
+                case msg_nav_controller_output.MAVLINK_MSG_ID_NAV_CONTROLLER_OUTPUT:
+                    hudWidget.setDistanceToWaypoint(((msg_nav_controller_output) msg).wp_dist);
+                    break;
+                case msg_heartbeat.MAVLINK_MSG_ID_HEARTBEAT:
+                    hudWidget.setMode(((msg_heartbeat) msg).custom_mode);
+                    break;
+                default:
+                    break;
+            }
+        }
+
+        @Override
+        public void notifyConnected() {
+            connectButton.setTitle(getResources().getString(
+                    R.string.menu_disconnect));
+
+            setupMavlinkStreamRate();
+        }
+
+        @Override
+        public void notifyDisconnected() {
+            connectButton.setTitle(getResources().getString(
+                    R.string.menu_connect));
+        }
+
+    };
+
+    private void setupMavlinkStreamRate() {
+        SharedPreferences prefs = PreferenceManager
+                .getDefaultSharedPreferences(this);
+        int rate = Integer.parseInt(prefs.getString("pref_mavlink_stream_rate",
+                "0"));
+        if (rate == 0) {
+            requestMavlinkDataStream(10, 0, false); // MAV_DATA_STREAM_RAW_CONTROLLER;
+        } else {
+            requestMavlinkDataStream(10, rate, true);
+        }
+    }
+
+    private void requestMavlinkDataStream(int stream_id, int rate, boolean start) {
+        msg_request_data_stream msg = new msg_request_data_stream();
+        msg.target_system = 1;
+        msg.target_component = 1;
+
+        msg.req_message_rate = (short) rate;
+        msg.req_stream_id = (byte) stream_id;
+
+        msg.start_stop = (byte) (start ? 1 : 0);
+        MAVClient.sendMavPacket(msg.pack());
+    }
+
+}*/
---
+++ app/src/main/java/com/drones/dimis/dronehud/TelemetryFragment.java
@@ -0,0 +1,172 @@
+package com.drones.dimis.dronehud;
+
+
+import android.app.Activity;
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import com.o3dr.services.android.lib.drone.property.State;
+import com.o3dr.services.android.lib.drone.property.VehicleMode;
+
+import java.util.List;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ * Activities that contain this fragment must implement the
+ * {@link com.drones.dimis.dronehud.TelemetryFragment.OnTelemetryFragmentInteractionListener} interface
+ * to handle interaction events.
+ * Use the {@link TelemetryFragment#newInstance} factory method to
+ * create an instance of this fragment.
+ */
+public class TelemetryFragment extends Fragment {
+    // TODO: Rename parameter arguments, choose names that match
+    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+    private static final String ARG_PARAM1 = "param1";
+    private static final String ARG_PARAM2 = "param2";
+
+    // TODO: Rename and change types of parameters
+    private int mParam1;
+    private String mParam2;
+
+    private OnTelemetryFragmentInteractionListener mListener;
+
+    Spinner modeSelector;//to fragment
+
+    /**
+     * Use this factory method to create a new instance of
+     * this fragment using the provided parameters.
+     *
+     * @param param1 Parameter 1.
+     * @param param2 Parameter 2.
+     * @return A new instance of fragment Telemetry.
+     */
+    // TODO: Rename and change types and number of parameters
+    public static TelemetryFragment newInstance(int param1, String param2) {
+        TelemetryFragment fragment = new TelemetryFragment();
+        Bundle args = new Bundle();
+        args.putInt(ARG_PARAM1, param1);
+        args.putString(ARG_PARAM2, param2);
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    public TelemetryFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        if (getArguments() != null) {
+            mParam1 = getArguments().getInt(ARG_PARAM1);
+            mParam2 = getArguments().getString(ARG_PARAM2);
+        }
+
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.telemetry_fragment, container, false);
+
+        return rootView;
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        this.modeSelector = (Spinner) getActivity().findViewById(R.id.modeSelect);
+        this.modeSelector.setOnItemSelectedListener(new Spinner.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                onFlightModeSelected(view);
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Do nothing
+            }
+        });
+
+        updateVehicleModesForType(mParam1);
+
+    }
+
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+
+    public void onFlightModeSelected(View view) {
+        VehicleMode vehicleMode = (VehicleMode) this.modeSelector.getSelectedItem();
+        if (mListener != null) {
+            mListener.onTelemetryFragmentInteraction(vehicleMode);
+        }
+        //
+    }
+
+    protected void updateVehicleModesForType(int droneType) {
+
+        List<VehicleMode> vehicleModes = VehicleMode.getVehicleModePerDroneType(droneType);
+        ArrayAdapter<VehicleMode> vehicleModeArrayAdapter;
+        vehicleModeArrayAdapter = new ArrayAdapter<VehicleMode>(getActivity(), android.R.layout.simple_spinner_item, vehicleModes);
+        vehicleModeArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        this.modeSelector.setAdapter(vehicleModeArrayAdapter);
+    }
+
+    protected void updateVehicleMode(State vehicleState) {
+        VehicleMode vehicleMode = vehicleState.getVehicleMode();
+        ArrayAdapter arrayAdapter = (ArrayAdapter) this.modeSelector.getAdapter();
+        this.modeSelector.setSelection(arrayAdapter.getPosition(vehicleMode));
+    }
+
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        try {
+            mListener = (OnTelemetryFragmentInteractionListener) activity;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(activity.toString()
+                    + " must implement OnTelemetryFragmentInteractionListener");
+        }
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+        mListener = null;
+    }
+
+    /**
+     * This interface must be implemented by activities that contain this
+     * fragment to allow an interaction in this fragment to be communicated
+     * to the activity and potentially other fragments contained in that
+     * activity.
+     * <p/>
+     * See the Android Training lesson <a href=
+     * "http://developer.android.com/training/basics/fragments/communicating.html"
+     * >Communicating with Other Fragments</a> for more information.
+     */
+    public interface OnTelemetryFragmentInteractionListener {
+        // TODO: Update argument type and name
+        public void onTelemetryFragmentInteraction(VehicleMode vehicleMode);
+    }
+
+}
